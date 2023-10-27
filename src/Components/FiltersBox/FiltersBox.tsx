8ad362d242e8bdc0c98596a9ac6334baed238672
liorpr <liorpr7@gmail.com>
2023-10-27 18:53:59 +0300

    more filters

--- src/Components/FiltersBox/FiltersBox.tsx
+++ src/Components/FiltersBox/FiltersBox.tsx
@@ -27,7 +27,16 @@ type SkillsFilter = {
     onOpenChange: (open: boolean) => void;
 };
 
-type Filter = YesNoFilter | SkillsFilter;
+type KeywordFilter = {
+    type: 'Keyword';
+    title: string;
+    isActive: boolean;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+    onOpenChange: (open: boolean) => void;
+};
+
+type Filter = YesNoFilter | SkillsFilter | KeywordFilter;
 
 interface FiltersBoxProps {
     filters: Filter[];
@@ -42,7 +51,7 @@ export function FiltersBox({ filters }: FiltersBoxProps) {
             <ul className="flex justify-center gap-2">
                 {filters.map((filter) => (
                     <li key={filter.title}>
-                        <Popover onOpenChange={filter?.onOpenChange}>
+                        <Popover onOpenChange={filter.onOpenChange}>
                             <PopoverTrigger asChild>
                                 <Button
                                     variant="ghost"
@@ -58,7 +67,13 @@ export function FiltersBox({ filters }: FiltersBoxProps) {
                                     </span>
                                 </Button>
                             </PopoverTrigger>
-                            <PopoverContent className="w-80">
+                            <PopoverContent
+                                className={cn(
+                                    filter.type === 'Skills'
+                                        ? 'w-[75vh]'
+                                        : 'w-80'
+                                )}
+                            >
                                 {filter.type === 'YesNo' && (
                                     <Toggle
                                         options={[
@@ -76,44 +91,62 @@ export function FiltersBox({ filters }: FiltersBoxProps) {
                                     />
                                 )}
                                 {filter.type == 'Skills' && (
-                                    <Autocomplete
-                                        id="skills"
-                                        multiple
-                                        disableCloseOnSelect
-                                        value={filter.value}
-                                        onChange={(_, newValue) => {
-                                            filter.setValue(newValue);
-                                        }}
-                                        options={filter.allSkills}
-                                        getOptionLabel={(option) => option.name}
-                                        renderOption={(props, option) => (
-                                            <li {...props}>
-                                                <Checkbox
-                                                    icon={
-                                                        <CheckBoxOutlineBlankIcon fontSize="small" />
-                                                    }
-                                                    checkedIcon={
-                                                        <CheckBoxIcon fontSize="small" />
-                                                    }
-                                                    style={{ marginRight: 8 }}
-                                                    checked={
-                                                        !!filter.value.find(
-                                                            (curr) =>
-                                                                curr.id ===
-                                                                option.id
-                                                        )
-                                                    }
+                                    <div className="flex justify-center">
+                                        <Autocomplete
+                                            id="skills"
+                                            multiple
+                                            disableCloseOnSelect
+                                            value={filter.value}
+                                            onChange={(_, newValue) => {
+                                                filter.setValue(newValue);
+                                            }}
+                                            options={filter.allSkills}
+                                            getOptionLabel={(option) =>
+                                                option.name
+                                            }
+                                            renderOption={(props, option) => (
+                                                <li {...props}>
+                                                    <Checkbox
+                                                        icon={
+                                                            <CheckBoxOutlineBlankIcon fontSize="small" />
+                                                        }
+                                                        checkedIcon={
+                                                            <CheckBoxIcon fontSize="small" />
+                                                        }
+                                                        style={{
+                                                            marginRight: 8,
+                                                        }}
+                                                        checked={
+                                                            !!filter.value.find(
+                                                                (curr) =>
+                                                                    curr.id ===
+                                                                    option.id
+                                                            )
+                                                        }
+                                                    />
+                                                    {option.name}
+                                                </li>
+                                            )}
+                                            style={{ width: 400 }}
+                                            renderInput={(params) => (
+                                                <TextField
+                                                    {...params}
+                                                    label="Skills"
                                                 />
-                                                {option.name}
-                                            </li>
-                                        )}
-                                        style={{ width: 400 }}
-                                        renderInput={(params) => (
-                                            <TextField
-                                                {...params}
-                                                label="Skills"
-                                            />
-                                        )}
+                                            )}
+                                        />
+                                    </div>
+                                )}
+                                {filter.type === 'Keyword' && (
+                                    <TextField
+                                        id="keyword"
+                                        label="Keywords"
+                                        variant="standard"
+                                        style={{ height: 'fit-content' }}
+                                        value={filter.value}
+                                        onChange={(e) => {
+                                            filter.setValue(e.target.value);
+                                        }}
                                     />
                                 )}
                             </PopoverContent>
